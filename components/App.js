d92b025821eca8800fb379dbd98d943ffcb53d16
Jari Bakken <jari@holderdeord.no>
2016-01-31 14:40:11 +0100

    Add tracking

--- components/App.js
+++ components/App.js
@@ -8,6 +8,14 @@ import Body from './Body';
 import Footer from './Footer';
 
 export default class App extends Component {
+    componentDidMount() {
+        if (process.env.NODE_ENV === 'production' && window.ga) {
+            window.ga('create', 'UA-19569290-8', 'auto');
+            window.ga('set', 'anonymizeIp', true);
+            window.ga('send', 'pageview');
+        }
+    }
+
     render() {
         return (
             <div id='App' className="container">
