8066ce46a2c6af04eecf6f0c631148ff74db16f9
Jari Bakken <jari@holderdeord.no>
2016-02-08 22:59:02 +0100

    Indentation fixes.

--- components/Chart.js
+++ components/Chart.js
@@ -2,53 +2,52 @@ import React, { Component, PropTypes } from 'react';
 import Highcharts from 'highcharts';
 
 export default class Chart extends Component {
-     propTypes: {
-          config: PropTypes.object.isRequired,
-          isPureConfig: PropTypes.bool
-     };
+    propTypes: {
+     config: PropTypes.object.isRequired,
+     isPureConfig: PropTypes.bool
+    };
 
-     renderChart(config = this.props.config) {
-          if (!config) {
-               throw new Error('Config must be specified for the Chart component');
-          }
-  
-          let chartConfig = config.chart;
+    renderChart(config = this.props.config) {
+        if (!config) {
+            throw new Error('Config must be specified for the Chart component');
+        }
 
-          if (this.chart && this.props.singleSeriesChange) {
-               this.chart.series[0].setData(config.series[0].data, true);
-          } else {
-               this.chart = new Highcharts.Chart({
-                   ...config,
-                   chart: {
-                     ...chartConfig,
-                     renderTo: this.refs.chart
-                   }
-               });
-          }
-  
-     }
+        let chartConfig = config.chart;
 
-     shouldComponentUpdate(nextProps) {
-          return !this.props.isPureConfig || !(this.props.config === nextProps.config)
-     }
+        if (this.chart && this.props.singleSeriesChange) {
+            this.chart.series[0].setData(config.series[0].data, true);
+        } else {
+            this.chart = new Highcharts.Chart({
+                ...config,
+                chart: {
+                    ...chartConfig,
+                    renderTo: this.refs.chart
+                }
+            });
+        }
+    }
 
-     componentDidUpdate() {
-          this.renderChart();
-     }
+    shouldComponentUpdate(nextProps) {
+        return !this.props.isPureConfig || !(this.props.config === nextProps.config)
+    }
 
-     getChart() {
-          if (!this.chart) {
+    componentDidUpdate() {
+        this.renderChart();
+    }
+
+    getChart() {
+        if (!this.chart) {
             throw new Error('getChart() should not be called before the component is mounted');
-          }
+        }
 
-          return this.chart;
-     }
+        return this.chart;
+    }
 
-     componentDidMount() {
-          this.renderChart();
-     }
+    componentDidMount() {
+        this.renderChart();
+    }
 
-     render() {
-          return <div ref="chart" />;
-     }
+    render() {
+        return <div ref="chart" />;
+    }
 };
